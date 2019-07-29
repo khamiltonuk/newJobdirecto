3a9c69e43e76ddff878b89d9e00b2d84af0dd897
Wilfredo <casas.farach@gmail.com>
2019-07-29 23:20:36 +0200

    a

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(
 
 passport.use(new FacebookStrategy({
         clientID: 1227008554140703,
-        clientSecret: secrets.facebookSecret,
+        clientSecret: fbSecret.facebookSecret,
         callbackURL: "http://localhost:8080/facebook/callback"
     },
     function(accessToken, refreshToken, profile, done) {
