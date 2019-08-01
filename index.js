a1a5e3c8be0d8068c73a847adb31c09c9a31d927
Wilfredo <casas.farach@gmail.com>
2019-08-01 13:18:31 +0200

    fix attempt to serialize problem

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ app.use(
 passport.use(new FacebookStrategy({
         clientID: 1227008554140703,
         clientSecret: fbSecret,
-        callbackURL: "http://localhost:8080/facebook/callback"
-        // callbackURL: "https://staging-jobdirecto.herokuapp.com/facebook/callback"
+        // callbackURL: "http://localhost:8080/facebook/callback"
+        callbackURL: "https://staging-jobdirecto.herokuapp.com/facebook/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         return database.findOrCreateFacebookUser(profile.id, profile.displayName).then((user) => {
